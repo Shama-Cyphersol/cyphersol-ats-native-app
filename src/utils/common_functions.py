a0c4ddf2d79b977e5ef354f3bf7367d583e24b3e
Manish Patil <94184147+Manya009@users.noreply.github.com>
2024-11-04 15:17:36 +0530

    Update common_functions.py

--- src/utils/common_functions.py
+++ src/utils/common_functions.py
@@ -458,7 +458,7 @@ class CommonFunctions:
                 last_non_zero_row = non_zero_rows.iloc[-1]
                 closing_bal[column] = last_non_zero_row[column]
 
-        opening_bal_value_1 = df["Balance"][0]
+        opening_bal_value_1 = df.iloc[0]["Balance"]
         keys_list = list(closing_bal.keys())
         values_list = list(closing_bal.values())
         values_list.insert(0, opening_bal_value_1)
